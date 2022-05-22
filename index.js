a720dc2f7891fdd2adbae1e2a18b387d1baf04a2
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-22 17:02:43 +0600

    implement get and insert product api

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 const express = require("express");
-    const app = express();
-    const cors = require("cors");
-    const { MongoClient, ServerApiVersion } = require("mongodb");
-    const ObjectId = require('mongodb').ObjectId;
-    const port = process.env.PORT || 5000;
-    
-    require('dotenv').config();
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const port = process.env.PORT || 5000;
 
-    // use middleware
-    app.use(cors());
-    app.use(express.json());
+require("dotenv").config();
 
-    // for testing
-    app.get("/", (req, res) => {
-        res.send({ message: "Success" });
-    });
+// use middleware
+app.use(cors());
+app.use(express.json());
 
-    app.listen(port, () => {
-        console.log("Listening to port", port);
-    });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zp9wl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+      await client.connect();
+      // Collections
+      const productsCollection = client.db("dewalt_DB").collection("products");
+  
+      // Get all products
+      app.get("/product", async (req, res) => {
+        const query = req.query;
+        const products = await productsCollection.find(query).toArray();
+        console.log("mongodb connected");
+        res.send(products);
+      });
+
+      // Add new product
+      app.post("/add-product", async(req, res) =>{
+          const data = req.body;
+          const addedProduct = await productsCollection.insertOne(data);
+          res.send(addedProduct)
+      })
+    } finally {
+      //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+// for testing
+app.get("/", (req, res) => {
+  res.send({ message: "Success" });
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
