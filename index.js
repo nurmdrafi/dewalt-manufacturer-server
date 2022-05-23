2f9c3f5702d45ef7906b0bca9bbd3ee8698a8a3e
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-23 12:48:47 +0600

    integrate review api

--- index.js
+++ index.js
@@ -21,42 +21,63 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
     // Collections
-    const productsCollection = client.db("dewalt_DB").collection("products");
+    const productCollection = client.db("dewalt_DB").collection("products");
+    const reviewCollection = client.db("dewalt_DB").collection("reviews");
 
     // Get all products
     // http://localhost:5000/product
     app.get("/product", async (req, res) => {
       const query = req.query;
-      const products = await productsCollection.find(query).toArray();
+      const products = await productCollection.find(query).toArray();
       console.log("mongodb connected");
       res.send(products);
     });
-    
+
     // Get product by id
-    // http://localhost:5000/product/:id
+    // http://localhost:5000/product/${_id}
     app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const product = await productsCollection.find(query).toArray();
+      const product = await productCollection.find(query).toArray();
       res.send(product);
     });
 
     // Add new product
-    //   http://localhost:5000/add-product
+    // http://localhost:5000/add-product
     app.post("/add-product", async (req, res) => {
       const data = req.body;
-      const addedProduct = await productsCollection.insertOne(data);
+      const addedProduct = await productCollection.insertOne(data);
       res.send(addedProduct);
     });
 
     // Delete product by id
-    app.delete("/product/:id", async (req, res) =>{
+    // `http://localhost:5000/product/${selectedId}
+    app.delete("/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const deletedProduct = await productsCollection.deleteOne(query);
+      const deletedProduct = await productCollection.deleteOne(query);
       res.send(deletedProduct);
+    });
+
+    // Get all Reviews
+    // http://localhost:5000/reviews
+    app.get("/reviews", async(req, res) =>{
+      const query = req.query;
+      const reviews = await reviewCollection.find(query).toArray();
+      res.send(reviews);
     })
+
+    // Add review
+    // http://localhost:5000/add-review
+    app.post("/add-review", async (req, res) => {
+      const data = req.body;
+      const insertedReview = await reviewCollection.insertOne(data);
+      res.send(insertedReview);
+    });
+
+
   } finally {
     //   await client.close();
   }
