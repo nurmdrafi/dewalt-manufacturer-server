cf93028665ef319899ba56b0a3a4d26d593746a2
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-23 19:20:18 +0600

    create api for issue token and set userCollection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
@@ -56,6 +57,24 @@ async function run() {
         res.status(403).send({ message: "Forbidden Access" });
       }
     };
+
+    // Issue Token
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
+    });
 
     // Get all products
     // http://localhost:5000/product
